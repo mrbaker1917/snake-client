74dcab2f09501d1251440b546f50145dd0b9a2a0
Mark Baker <mrbaker1917@gmail.com>
2020-08-26 22:00:33 +0000

    modified play.js with setupInput and handleUserInput

--- play.js
+++ play.js
@@ -1,3 +1,23 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    console.log('Hope you enjoyed the game! Ciao!');
+    process.exit();
+  }
+};
+
+const setupInput = function(data) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+
+
+  return stdin;
+};
+setupInput();
+
